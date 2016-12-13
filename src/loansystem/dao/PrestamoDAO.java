5239bd5918768e45b299e5cbb10af9bc67eea230
Claver <ciga282@gmail.com>
2016-12-13 00:28:36 -0600

    Creación de la clase PrestamoDAO

---
+++ src/loansystem/dao/PrestamoDAO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loansystem.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author jechavarria
+ */
+public class PrestamoDAO {
+private Connection con;
+private ResultSet rs;
+private Statement s;
+
+public PrestamoDAO(Connection con) {
+        this.con = con;
+}
+
+//Obtener todos los abonos de un cliente a un prestamos especifico
+
+//
+    
+    
+ 
+}
